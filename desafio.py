1ac4264bcb92f5e089d119c47ebdbcbaddc8024d
Marcelo Oliveira <celo10091@gmail.com>
2023-07-16 16:58:30 +0100

    Create desafio.py

---
+++ desafio.py
@@ -0,0 +1,18 @@
+menu = """
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+!! Escolha uma opção acima!!
+
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 3
+
+while True:
+
+    opcao = input(menu)
+     if opcao == "d":
